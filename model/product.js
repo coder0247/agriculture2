7df1ba7cba3dd334460d740d2a0c85835f1625c0
coder0247@gmail.com <coder0247@gmail.com>
2018-11-07 10:07:02 +0530

    updated htaccess on heroku

--- model/product.js
+++ model/product.js
@@ -29,7 +29,7 @@ const Product = mongoose.model('Products', productSchema);
 productSchema.plugin(autoIncrement.plugin, {
     model: Product,
     field: 'adid',
-    startAt: 1201,
+    startAt: 1601,
     incrementBy: 1
 });
 module.exports = Product;
