3b12c6d48403d10eea9fb447bed08da36f650226
coder0247 <coder0247@gmail.com>
2018-11-21 13:34:47 +0530

    update code

--- model/product.js
+++ model/product.js
@@ -29,7 +29,7 @@ const Product = mongoose.model('Products', productSchema);
 productSchema.plugin(autoIncrement.plugin, {
     model: 'Products',
     field: 'adid',
-    startAt: 1601,
+    startAt: 1700,
     incrementBy: 1
 });
 module.exports = Product;
