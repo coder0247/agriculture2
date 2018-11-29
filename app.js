fdae556561220b1a9735a3029fbd1cf5a11c6fc3
coder0247 <coder0247@gmail.com>
2018-11-29 20:40:40 +0530

    currency and ie issue fixed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const config = require('./config');
 const headerRoute = require('./routes/header');
 const footerRoute = require('./routes/footer');
 const userRoute = require('./routes/user');
-const homeRoute = require('./routes/home'); 
+const homeRoute = require('./routes/home');
 const accountRoute = require('./routes/account');
 const categoryRoute = require('./routes/category');
 const contactRoute = require('./routes/contact');
@@ -90,4 +90,4 @@ app.use(adMostViewedProduct);
 app.use(adNewArrivalsProduct);
 app.use(adOnSaleProduct);
 app.use(adHomeProductCatwiseStatus);
-app.listen(3000, () => console.log('server running on port 3000!'))
+app.listen(process.env.PORT || 4001)
